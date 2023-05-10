3843992699deba89098098e29c33e5e6bfde5ee8
Lenovo <lenovo@ubuntu.ubuntu-domain>
2023-05-10 23:12:42 +0300

    Update test to be Qt app

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -1,7 +1,18 @@
 #include "tests.cpp"
 #include <gtest/gtest.h>
+#include <QCoreApplication>
+#include <QTimer>
 
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char *argv[])
+{
+    QCoreApplication app{argc, argv};
+
+    QTimer::singleShot(0, [&]()
+    {
+        ::testing::InitGoogleTest(&argc, argv);
+        auto testResult = RUN_ALL_TESTS();
+        app.exit(testResult);
+    });
+
+    return app.exec();
 }
